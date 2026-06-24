f5b2f874d776bce5e1072b500121e27a359a1167
HIRWA KAYIHURA Kevin <hirwakayihurakevin@gmail.com>
2026-06-24 12:17:37 +0200

    Feature/Hamburger-menu-mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCartShopping,
+  faBars,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import "../styles/Navbar.css";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav>
       <div className="nav-content">
@@ -10,16 +17,33 @@ export default function Navbar() {
           <img src="/logo.png" alt="Our Kithen Logo" id="logo" />
           <p>KitchenBasics</p>
         </div>
-        <div className="nav-links">
-          <a href="#">Cookware</a>
-          <a href="#">Tableware</a>
-          <a href="#">Appliances</a>
-          <a href="#">About</a>
+        <div className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+          <a href="#" onClick={() => setIsMenuOpen(false)}>
+            Cookware
+          </a>
+          <a href="#" onClick={() => setIsMenuOpen(false)}>
+            Tableware
+          </a>
+          <a href="#" onClick={() => setIsMenuOpen(false)}>
+            Appliances
+          </a>
+          <a href="#" onClick={() => setIsMenuOpen(false)}>
+            About
+          </a>
         </div>
-        <div>
+        <div className="nav-actions">
           <a href="#" id="cart-icon">
             <FontAwesomeIcon icon={faCartShopping} />
           </a>
+          <button
+            type="button"
+            className="hamburger"
+            aria-label="Toggle navigation menu"
+            aria-expanded={isMenuOpen}
+            onClick={() => setIsMenuOpen((open) => !open)}
+          >
+            <FontAwesomeIcon icon={isMenuOpen ? faXmark : faBars} />
+          </button>
         </div>
       </div>
     </nav>
